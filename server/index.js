eb7d5aebf76c1708e3b3b331600ffa79b50ed29c
Ostap Blystsiv <ostap.blystsiv@gmail.com>
2023-03-30 18:30:21 +0300

    feat: add data and update dependencies

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
+import mongoose from 'mongoose';
 import morgan from "morgan";
 
 /* CONFIGURATIONS */
@@ -17,3 +18,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 7777
+
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).then(
+		async () => {
+			app.listen(PORT, () => console.log(`Server on: http://localhos:${PORT}`))
+		}
+	).catch((err) => console.log(err))
